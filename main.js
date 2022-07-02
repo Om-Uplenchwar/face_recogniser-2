a1fc78f950ad14f8f9c1654df42f0d7c5a6c52f0
Om-Uplenchwar <81371929+Om-Uplenchwar@users.noreply.github.com>
2022-07-02 14:07:34 +0530

    Update main.js

--- main.js
+++ main.js
@@ -31,9 +31,3 @@ function gotResult(error, results) {
     if (error) {
         console.error(error)
     }
-    else {
-        console.log(results);
-        document.getElementById("result_name").innerHTML = results[0].label;
-        document.getElementById("result_accuracy").innerHTML = results[0].confidence.toFixed(3);   
-    }
-}
